5734c9c6ae7e94c973d763a739230875d753a185
kaiwxn <kwyang0620@icloud.com>
2024-03-24 15:50:02 +0100

    crap

--- binnet/binnetapp/calculate.py
+++ binnet/binnetapp/calculate.py
@@ -1,6 +1,29 @@
+import datetime
 
+def splitWeekDay(measurements):
+    # Split measurements into weekdays
+    ans = []
+    for measurement in measurements:
+        if measurement.measureDate.getWeekday() == datetime.datetime.now().weekday():
+            ans.append(measurement)
+    return 
 
 def calculateRanking(measurements) -> str:
+
+    # 1. Calculate increasing parts of the measurements
+    # 2. Calculate average of those squences
+    # 4. Return color based on average
+
+    # 1.
+    increasingSublists = [[measurements[0].value]]
+
+    for i in range(1, len(measurements)):
+        if measurements[i-1].value < measurements[i].value or abs(measurements[i-1].value - measurements[i].value) < 10:
+            increasingSublists.append(measurements[i].value)
+        else:
+            increasingSublists.append([measurements[i].value])
+
+
 
     
     return
