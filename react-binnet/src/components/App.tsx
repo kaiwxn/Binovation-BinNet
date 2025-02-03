4bee39f2793086b79c575aaaec357a43855f63e0
kaiwxn <kwyang0620@icloud.com>
2025-02-03 20:21:59 +0100

    prettier

--- react-binnet/src/components/App.tsx
+++ react-binnet/src/components/App.tsx
@@ -1,28 +1,32 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-    const [data, setData] = useState(0)
+	const [data, setData] = useState(0);
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/binnetapp/')
-        .then((res) => {
-            setData(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }, [])
+	useEffect(() => {
+		axios
+			.get("http://localhost:8000/binnetapp/")
+			.then((res) => {
+				setData(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    return (
-      <div>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
-      <h1 className="text-3xl font-bold underline p-10"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, veniam?</h1>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
-      <input type="checkbox" aria-label="Checkbox" className="btn" />
-      </div>
-    );
+	return (
+		<div>
+			{data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."}
+			<h1 className="text-3xl font-bold underline p-10">
+				{" "}
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, veniam?
+			</h1>
+			<button className="btn btn-outline btn-secondary">Secondary</button>
+			<input type="checkbox" aria-label="Checkbox" className="btn" />
+		</div>
+	);
 }
 
-export default App
+export default App;
